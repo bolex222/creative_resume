9f625d32afe8b247876850306312b53a744ebb93
Basile LECOUTURIER <lecouturier.basile@gmail.com>
2022-11-07 22:55:38 +0100

    adding eslint

---
+++ src/components/Buttons/ScrollDownButton.tsx
@@ -0,0 +1,49 @@
+import React, { useLayoutEffect, useRef } from 'react'
+import { gsap } from 'gsap'
+
+import styles from './ScrollDownButton.module.scss'
+
+function ScrollDownButton() {
+  const buttonRef = useRef(null)
+  const arrowRef = useRef(null)
+
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      const t1 = gsap.timeline()
+      t1.set(arrowRef.current, { opacity: 0, translateY: '-100%' })
+      t1.fromTo(
+        buttonRef.current,
+        { scale: 0 },
+        { scale: 1, duration: 0.6, delay: 1, ease: 'power1.out' }
+      )
+      t1.to(
+        arrowRef.current,
+        { opacity: 1, translateY: 0, duration: 0.6, ease: 'power1.out' },
+        '-=0.3'
+      )
+    })
+    return () => ctx.revert()
+  }, [])
+
+  return (
+    <div className={styles.scrollCircleWrapper}>
+      <div ref={buttonRef} className={styles.scrollCircle}>
+        <h3 className={styles.scrollCircleHeading}>scroll</h3>
+      </div>
+      <svg
+        ref={arrowRef}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        className={styles.scrollCircleSvg}
+        viewBox="0 0 16 16"
+      >
+        <path
+          fillRule="evenodd"
+          d="M1.553 6.776a.5.5 0 0 1 .67-.223L8 9.44l5.776-2.888a.5.5 0 1 1 .448.894l-6 3a.5.5 0 0 1-.448 0l-6-3a.5.5 0 0 1-.223-.67z"
+        />
+      </svg>
+    </div>
+  )
+}
+
+export default ScrollDownButton
--- src/pages/home/Hero/HeroSection.tsx
+++ src/pages/home/Hero/HeroSection.tsx
@@ -1,22 +1,25 @@
 import React from 'react'
+import AnimatedText from '@/components/text/AnimatedText'
+import YearSvg from '@/components/svg/YearSvg'
+import HeroBackground from '@/pages/home/Hero/HeroBackground'
 import styles from './HeroSection.module.scss'
-import AnimatedText from "@/components/text/AnimatedText";
-import YearSvg from "@/components/svg/YearSvg";
-import HeroBackground from "@/pages/home/Hero/HeroBackground";
+import ScrollDownButton from '@/components/Buttons/ScrollDownButton'
 
 const HeroSection = () => {
-    return (
-        <section className={styles.hero_section}>
-            <HeroBackground/>
-            <h1 className={styles.title}>
-                <AnimatedText text="Basile LECOUTURIER"/>
-            </h1>
-            <div>
-                <YearSvg/>
-            </div>
-            <div className={styles.grainyOverlayContainer}/>
-        </section>
-    )
+  return (
+    <section className={styles.hero_section}>
+      <HeroBackground />
+      <h1 className={styles.title}>
+        <AnimatedText text="Basile LECOUTURIER" />
+      </h1>
+      {/*year svg container for apparition animation*/}
+      <div>
+        <YearSvg />
+      </div>
+      <ScrollDownButton />
+      <div className={styles.grainyOverlayContainer} />
+    </section>
+  )
 }
 
 export default HeroSection
